19a0d7ee29a47a346118552d8dfd9ef07462d043
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-10-25 22:10:41 +0600

    Used Multer to save file and created uploadFile.js to use as a middleware and added in processRegister route.

---
+++ src/middlewares/uploadFile.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+const path = require("path");
+const { uploadDir, maximumFileSize, allowedFileTypes } = require("../secret");
+const createError = require("http-errors");
+const maxFileSize = Number(maximumFileSize);
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const extName = path.extname(file.originalname);
+    cb(null, Date.now() + "-" + file.originalname.replace(extName, "") + extName);
+    console.log(maxFileSize);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+    const extName = path.extname(file.originalname);
+    if (!allowedFileTypes.includes(extName.substring(1))) {
+        return cb(createError(422, "Only image files are allowed"));
+    } 
+    cb(null, true);
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter, limits: { fileSize: maxFileSize } }); 
+
+module.exports = upload;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const { getUsers, getUserByID, deleteUserByID, processRegister, activateUserAccount } = require('../controllers/userController');
+const upload = require('../middlewares/uploadFile');
 const userRouter = express.Router();
 
 
@@ -7,7 +8,7 @@ const userRouter = express.Router();
 userRouter.get("/", getUsers);
 userRouter.get('/:id', getUserByID);
 userRouter.delete('/:id', deleteUserByID);
-userRouter.post("/process-register", processRegister);
+userRouter.post("/process-register", upload.single('image'), processRegister);
 userRouter.post("/verify", activateUserAccount);
 
 module.exports = userRouter;
